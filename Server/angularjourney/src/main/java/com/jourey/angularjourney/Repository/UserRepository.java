c1e1828b503944a6a4df554d497545c04615875b
rio1110 <ryo.apple.tokyo3871@gmail.com>
2019-04-10 01:12:47 +0900

    git cache削除

--- Server/angularjourney/src/main/java/com/jourey/angularjourney/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jourey.angularjourney.Repository;
-
-import com.jourey.angularjourney.Entity.User;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends CrudRepository<User, Long>{}
